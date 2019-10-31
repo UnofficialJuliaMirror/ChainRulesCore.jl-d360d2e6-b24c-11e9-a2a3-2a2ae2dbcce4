e1842cf253e0ea79d8106896e306941fd17fec4d
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-10-31 21:45:09 +0000

    make elementwise add

--- src/composite_backing.jl
+++ src/composite_backing.jl
@@ -40,9 +40,6 @@ function construct(::Type{T}, fields::NamedTuple{L}) where {T, L}
     end
 
     if @generated
-        if !isempty(setdiff(L, fieldnames())
-            ArgumentError("Extra fields in namedtuple (has$L), not ")
-        end
         vals = (:(getproperty(fields, $(QuoteNode(fname)))) for fname in fieldnames(T))
         return :(T($(vals...)))
     else
@@ -61,13 +58,15 @@ Directly constructs an object of type `T`, with the given fields.
 function directly_construct(::Type{T}, fields::NamedTuple{L}) where {T, L}
     #TODO based on
     #https://github.com/JuliaIO/BSON.jl/blob/a58c88a14e07d0beed8f56edb79e5cbea7078e00/src/extensions.jl#L107
+    # We need to use this rather than `construct` so that closures work.
     error("no implemented")
 end
 
 
 ########################################################################################
+elementwise_add(a::Tuple, b::Tuple) = map(+, a, b)
 
-function elementwise_add(a::NamedTuple{an}, b::NamedTuple{bn})
+function elementwise_add(a::NamedTuple{an}, b::NamedTuple{bn}) where {an, bn}
     # Base on the `merge(:;NamedTuple, ::NamedTuple)` code from Base.
     # https://github.com/JuliaLang/julia/blob/592748adb25301a45bd6edef3ac0a93eed069852/base/namedtuple.jl#L220-L231
     if @generated
@@ -75,8 +74,8 @@ function elementwise_add(a::NamedTuple{an}, b::NamedTuple{bn})
         types = Base.merge_types(names, a, b)
 
         vals = map(names) do field
-            a_field = :(getproperty(:a, $(QuoteNode(field))))
-            b_field = :(getproperty(:b, $(QuoteNode(field))))
+            a_field = :(getproperty(a, $(QuoteNode(field))))
+            b_field = :(getproperty(b, $(QuoteNode(field))))
             val_expr = if Base.sym_in(field, an)
                 if Base.sym_in(field, bn)
                     # in both
@@ -94,66 +93,23 @@ function elementwise_add(a::NamedTuple{an}, b::NamedTuple{bn})
         names = Base.merge_names(an, bn)
         types = Base.merge_types(names, typeof(a), typeof(b))
         vals = map(names) do field
-            val_expr = if Base.sym_in(field, an)
+            if Base.sym_in(field, an)
                 a_field = getproperty(a, field)
                 if Base.sym_in(field, bn)
                     # in both
-                    b_field = getproperty(a, field)
-                    :($a_field + $b_field)
+                    b_field = getproperty(b, field)
+                    a_field + b_field
                 else
                     # only in `an`
                     a_field
                 end
             else # must be in `b` only
-                b_field = getproperty(a, field)
+                b_field = getproperty(b, field)
                 b_field
             end
         end
-        NamedTuple{names,types}(map(n->getfield(sym_in(n, bn) ? b : a, n), names))
+        return NamedTuple{names,types}(vals)
     end
 end
 
-function Base.:+(a::Primal, b::Composite{Primal, <:NamedTuple})::Primal where {Primal}
-
-    backing =  #TODO: factor out the +(::Composite,::Composite) above and call it here.
-
-    return _construct(Primal, backing)  # should use _directly_construct
-end
-
-
-# this should not need to be generated, # TODO test that
-function Base.:+(a::Composite{Primal, <:Tuple}, b::Composite{Primal, <:Tuple}) where Primal
-    # TODO: should we even allow it on different lengths?
-    short, long =  length(a) < length(b) ? (a.backing, b.backing) : (b.backing, a.backing)
-    backing = ntuple(length(long)) do ii
-        long_val = getfield(long, ii)
-        if ii <= length(short)
-            short_val = getfield(short, ii)
-            return short_val + long_val
-        else
-            return long_val
-        end
-    end
-
-    return Composite{Primal, typeof(backing)}(backing)
-end
-
-
-# this should not need to be generated, # TODO test that
-function Base.:+(a::Primal, b::Composite{Primal, <:Tuple})::Primal where {Primal}
-    @assert Primal <: Tuple  # only Composites for Tuples have Tuple backing
-
-    # TODO: should we even allow it on different lengths?
-    short, long =  length(a) < length(b) ? (a.backing, b.backing) : (b.backing, a.backing)
-    return ntuple(length(long)) do ii
-        long_val = getfield(long, ii)
-        if ii <= length(short)
-            short_val = getfield(short, ii)
-            return short_val + long_val
-        else
-            return long_val
-        end
-    end
-end
-
-Base.:+(b::Composite{Primal}, a::Primal) where Primal =  a + b
+elementwise_add((y=25,),  (x=2, y=4))
